c34dd880f0bf1410c26389c39deae6b00e19db12
Taeyong123123 <56382912+faker-of-coding@users.noreply.github.com>
2025-06-06 16:35:40 +0900

    chap3_question1~11

---
+++ 중급1/String-클래스/문제8/문제8_배태용/TestString8.java
@@ -0,0 +1,7 @@
+public class TestString8 {
+    public static void main(String[] args) {
+        String input = "hello java spring jpa java";
+        // 코드 작성
+        System.out.println(input.replace("java","jvm"));
+    }
+}
