05d77c6f91fe799579bbb1564d1f81c760af33c2
Taeyong123123 <56382912+faker-of-coding@users.noreply.github.com>
2025-06-06 17:12:58 +0900

    chap4_question1~5

---
+++ 중급1/래퍼-Class-클래스/문제1/문제1_배태용/WrapperTest1.java
@@ -0,0 +1,8 @@
+public class WrapperTest1 {
+    public static void main(String[] args) {
+        String str1 = "10";
+        String str2 = "20";
+        // 코드 작성
+        System.out.println("두 수의 합: " + (Integer.parseInt(str1) + Integer.parseInt(str2)));
+    }
+}
