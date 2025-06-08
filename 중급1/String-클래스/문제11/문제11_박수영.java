db6c4bee8e8d0ef312601711c83fa471eed37cd1
박수영 <qkrtudwkd@gmail.com>
2025-06-08 20:58:13 +0900

    1주차 : 숙제업로드

--- 중급1/String-클래스/문제11/문제11_박수영.java
+++ 중급1/String-클래스/문제11/문제11_박수영.java
@@ -2,6 +2,6 @@ public class 문제11_박수영 {
     public static void main(String[] args) {
         String str = "Hello Java";
         String reversed = new StringBuilder(str).reverse().toString();
-        System.out.println(reversed); 
+        System.out.println(reversed);
     }
 }
